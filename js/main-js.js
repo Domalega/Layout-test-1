8fd83741369a6f36d5be9aeab7d010d3806f6ef6
Domalega <vladimir.domalega@gmail.com>
2021-08-11 01:23:46 +0700

    реализация адаптивного меню

--- js/main-js.js
+++ js/main-js.js
@@ -8,3 +8,16 @@
         }
     };
 }());
+
+(function() {
+    const menuOpenItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header__nav');
+    const menuCloseItem = document.querySelector('.header__burger-close')
+    menuOpenItem.addEventListener("click", () => {
+        menu.classList.add('header__nav_active');
+    }); {
+        menuCloseItem.addEventListener("click", () => {
+            menu.classList.remove('header__nav_active');
+        });
+    }
+}());
