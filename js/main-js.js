44d7c94edbcef7825ce9d3f169d9cd1033ba3181
Domalega <vladimir.domalega@gmail.com>
2021-08-12 21:33:03 +0700

    оптимизация css и js

--- js/main-js.js
+++ js/main-js.js
@@ -1,5 +1,5 @@
 (function() {
-    const header = document.querySelector(".header");
+    const header = document.querySelector('.header');
     window.onscroll = () => {
         if (window.pageYOffset > 100) {
             header.classList.add('header__active');
@@ -9,19 +9,28 @@
     };
 }());
 
+// Burger handler
 (function() {
-    const menuOpenItem = document.querySelector('.burger');
+    const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.header__nav');
-    const menuCloseItem = document.querySelector('.header__burger-close')
-    menuOpenItem.addEventListener("click", () => {
+    const menuCloseItem = document.querySelector('.header__burger-close');
+    const menuLinks = document.querySelectorAll('.header__link');
+    burgerItem.addEventListener('click', () => {
         menu.classList.add('header__nav_active');
-    }); {
-        menuCloseItem.addEventListener("click", () => {
-            menu.classList.remove('header__nav_active');
-        });
+    });
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header__nav_active');
+    });
+    if (window.innerWidth <= 767) {
+        for (let i = 0; i < menuLinks.length; i += 1) {
+            menuLinks[i].addEventListener('click', () => {
+                menu.classList.remove('header__nav_active');
+            });
+        }
     }
 }());
 
+// Scroll to anchors
 (function() {
 
     const smoothScroll = function(targetEl, duration) {
@@ -33,20 +42,17 @@
 
         const ease = function(t, b, c, d) {
             t /= d / 2;
-            if (t < 1)
-                c / 2 * t * t + b;
+            if (t < 1) return c / 2 * t * t + b;
             t--;
             return -c / 2 * (t * (t - 2) - 1) + b;
         };
 
         const animation = function(currentTime) {
-            if (startTime === null)
-                startTime = currentTime;
+            if (startTime === null) startTime = currentTime;
             const timeElapsed = currentTime - startTime;
             const run = ease(timeElapsed, startPosition, targetPosition, duration);
             window.scrollTo(0, run);
-            if (timeElapsed < duration)
-                requestAnimationFrame(animation);
+            if (timeElapsed < duration) requestAnimationFrame(animation);
         };
         requestAnimationFrame(animation);
 
